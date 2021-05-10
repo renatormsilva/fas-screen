9fc2f4197bc8af1de4b982c268c118c7cf21cf76
renatormsilva <renato.brtkm@gmail.com>
2021-05-10 00:44:05 -0400

    new commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 const inputs = document.querySelectorAll(".input");
+const forgotInput = document.getElementById("forgot");
+const forgotContent = document.getElementsByClassName("forgot-content")[0];
+const loginContent = document.getElementsByClassName("login-content")[0];
 
+forgotInput.onclick = function (e) {
+  e.preventDefault();
+  forgotContent.style.display = "flex";
+  loginContent.style.position = "absolute";
+  loginContent.style.display = "none";
+};
 
-function addcl(){
-	let parent = this.parentNode.parentNode;
-	parent.classList.add("focus");
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
 }
 
-function remcl(){
-	let parent = this.parentNode.parentNode;
-	if(this.value == ""){
-		parent.classList.remove("focus");
-	}
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
 }
 
-
-inputs.forEach(input => {
-	input.addEventListener("focus", addcl);
-	input.addEventListener("blur", remcl);
+inputs.forEach((input) => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
 });
